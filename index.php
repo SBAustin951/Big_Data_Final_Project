338dc8f3702dbbbc41ec0917f5d147b8f365f0bc
Chris Poppell <clpoppell@valdosta.edu>
2016-11-16 16:36:22 -0500

    Moved database search result display

--- index.php
+++ index.php
@@ -21,21 +21,64 @@
   <script type="text/javascript" src="js/gmaps.js"></script>
   <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=AIzaSyDAzxePfRURHOZ9FJgI1EHqWs_r3s_ypVo"></script>
   <script type="text/javascript">
-	
 	$(document).ready(function(){
 		<?php
+          if(isset($_GET['submit'])){
+            if(empty($_GET['key']) && empty($_GET['city']) && empty($_GET['stars'])){
+              echo "<p class='alert alert-warning' id='warning'>You Must Input At Least One Search Value</p>";
+            } else{
+              $keyWord= $_GET['key'];
+              $cityName= $_GET['city'];
+              $rating= $_GET['stars'];
+              $query= array('name' => new MongoRegex("/$keyWord/i"), 'city' => new MongoRegex("/$cityName/i"), 'stars' => array('$gte' => (float)$rating));
+              $result= $db->business->find($query)->sort(array('stars' => 1));
+
+			  echo "$('#results').html(\"";
+			  foreach($result as $r){
+				$address= str_replace(array("\r","\n"), " ", $r['full_address']);
+                echo "<div class='panel panel-default'>";
+					echo "<table class='table table-bordered'>";
+					echo "<thead class='thead-inverse'>";
+					echo "<tr><th class='well' colspan='2'>".$r['name']."</th></tr></thead>";
+					echo "<tbody><tr><td>Business Address</td>";
+					echo "<td><br/><strong>".$address."</strong></td></tr>";
+					echo "<tr><td align='justify'>Star Rating</td>";
+					echo "<td><br/><strong>".$r['stars']."</strong></td></tr>";
+					echo "<tr><td align='justify'>Link:</td>";
+					echo "<td><br/><strong><a href='https://twitter.com/search?q=".$keyWord."'>https://twitter.com/search?q=".$keyWord."</a></strong></td>";
+                echo "</tr></tbody></table></div>";
+              }
+			  echo "\");";
+            }
+		}
 			$lat= 51.5073346;
-			$lng= -0.1276831
+			$lng= -0.1276831;
+			
+			if(empty($_GET['city'])){} else{
+				$address= urlencode($_GET['city']);
+				$url= 'http://maps.googleapis.com/maps/api/geocode/json?address='.$address.'&sensor=false';
+				$geocode= file_get_contents($url);
+				$results= json_decode($geocode, true);
+			
+				if($results['status']=='OK'){
+					$lat= $results['results'][0]['geometry']['location']['lat'];
+					$lng= $results['results'][0]['geometry']['location']['lng'];
+				}
+			}
 		?>
-		var map= new GMaps({
-			div: '#map',
-		<?php
-			echo "lat: ".$lat.",\n";
-			echo "lng: ".$lng.",\n";
-		?>
-		});
+			var map= new GMaps({
+				div: '#map',
+				lat: <?php echo $lat ?>,
+				lng: <?php echo $lng ?>,
+			});
+			
+			map.addMarker({
+				lat: <?php echo $lat ?>,
+				lng: <?php echo $lng ?>,
+				title: "<?php echo $_GET['city'] ?>",
+			});
 	});
-		</script>
+  </script>
 </head>
 <body style="background-color: #6495ED">
   <div class="row">
@@ -55,35 +98,15 @@
   </div>
   <div class="row">
     <div class="container-fluid">
-      <div class="col-md-6" id="results">
-        <?php
-          if (isset($_GET['submit'])) {
-            if (empty($_GET['key']) && empty($_GET['city']) && empty($_GET['stars'])) {
-              echo "<p class='alert alert-warning' id='warning'>You Must Input At Least One Search Value</p>";
-            } else {
-              $keyWord = $_GET['key'];
-              $cityName = $_GET['city'];
-              $rating = $_GET['stars'];
-              $query = array('name' => new MongoRegex("/$keyWord/i"), 'city' => new MongoRegex("/$cityName/i"), 'stars' => array('$gte' => (float)$rating));
-              $result = $db->business->find($query)->sort(array('stars' => 1));
-
-              foreach ($result as $r) {
-                echo "<div class='panel panel-default'>";
-                echo "<table class='table table-bordered'>";
-                echo "<thead class='thead-inverse'>";
-                echo "<tr><th colspan='2'>".$r['name']."</th></tr></thead>";
-                echo "<tbody><tr><td>Business Address</td>";
-                echo "<td><br/><strong>".$r['full_address']."</strong></td></tr>";
-                echo "<tr><td align='justify'>Star Rating</td>";
-                echo "<td><br/><strong>".$r['stars']."</strong></td></tr>";
-                echo "<tr><td align='justify'>Link:</td>";
-                echo "<td><br/><strong><a href='https://twitter.com/search?q=".$keyWord."'>https://twitter.com/search?q=".$keyWord."</a></strong></td>";
-                echo "</tr></tbody></table></div>";
-              }
-            }
-          }
-        ?>
-      </div>
+      <div class="col-md-6" id="results"></div>
+	  <div class="col-md-6" id="map_container">
+		<h2 id="map_header">
+			<?php
+				if(empty($_GET['key']) && empty($_GET['city']) && empty($_GET['stars'])){}
+				else{ echo $_GET['key']." in ".$_GET['city']." with at least ".$_GET['stars']." stars"; }
+			?>
+		</h2>
+		<div id="map"></div>
     </div>
   </div>
 </body>
