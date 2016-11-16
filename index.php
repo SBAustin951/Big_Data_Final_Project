602b2d06e860ac58caa5f6b98f07a26c926ee9c5
Austin Underwood <sbaustin951@gmail.com>
2016-11-16 14:24:41 -0500

    Refactored if statements

--- index.php
+++ index.php
@@ -6,31 +6,30 @@
   //echo "Connected to Mongo successfully";
   $db = $m->$dbname;
   //echo "Database $dbname selected <br/>";
- ?>
+?>
 
 <html>
 <head>
-  <title>My Website</title>
+  <title>J.A.C. Reviews</title>
   <link href="css/style.css" rel="stylesheet" />
-
   <!-- import bootsrap -->
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script href="js/bootstrap.min.js"></script>
-
   <!-- import gmaps -->
   <script type="text/javascript" src="js/gmaps.js"></script>
 </head>
-<body>
+<body style="background-color: #6495ED">
   <div class="row">
     <nav class="navbar navbar-fixed-top navbar-dark bg-primary">
-      <div class="col-md-8">
-        <a class="navbar-brand" href="index.html" id="cname">Company Name</a>
+      <div class="col-md-6">
+        <a class="navbar-brand" href="index.php" id="cname">J.A.C. Reviews</a>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-6">
         <form method="get" class="form-inline" id="search">
           <input type="text" placeholder="Keyword" name="key" class="form-control" />
+          <input type="number" placeholder="Star Rating" name="stars" step="any" min="0.0" max="5.0" class="form-control" />
           <input type="text" placeholder="City Name" name="city" class="form-control" />
           <button type="submit" name="submit" class="btn btn-success">Search</button>
         </form>
@@ -40,113 +39,33 @@
   <div class="row">
     <div class="container-fluid">
       <div class="col-md-6" id="results">
-<?php
-          if (!empty($_GET['key']) && !empty($_GET['city'])) {
-            $keyWord = $_GET['key'];
-            $cityName = $_GET['city'];
-            $query = array('name' => new MongoRegex("/$keyWord/i"), 'city' => new MongoRegex("/$cityName/i"));
-            $result = $db->business->find($query)->sort(array('stars' => 1));
+        <?php
+          if (isset($_GET['submit'])) {
+            if (empty($_GET['key']) && empty($_GET['city']) && empty($_GET['stars'])) {
+              echo "<p class='alert alert-warning' id='warning'>You Must Input At Least One Search Value</p>";
+            } else {
+              $keyWord = $_GET['key'];
+              $cityName = $_GET['city'];
+              $rating = $_GET['stars'];
+              $query = array('name' => new MongoRegex("/$keyWord/i"), 'city' => new MongoRegex("/$cityName/i"), 'stars' => array('$gte' => (float)$rating));
+              $result = $db->business->find($query)->sort(array('stars' => 1));
 
-            foreach ($result as $r) {
-?>
-              <div class='panel panel-default'>
-                <table class='table table-bordered'>
-                  <thead class='thead-inverse'>
-                      <tr>
-                        <th colspan='2'><?php echo $r['name'] ?></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Business Address</td>
-                        <td><br/>
-                          <strong><?php echo $r['full_address'] ?></strong>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td align='justify'>Star Rating</td>
-                        <td><br/>
-                          <strong><?php echo $r['stars'] ?></strong>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-<?php
-            }
-          } else if (!empty($_GET['key'])) {
-            $keyWord = $_GET['key'];
-            $query = array('name' => new MongoRegex("/$keyWord/i"));
-            $result = $db->business->find($query)->sort(array('stars' => 1));
-
-            foreach ($result as $r) {
-?>
-                <div class='panel panel-default'>
-                  <table class='table table-bordered'>
-                    <thead class='thead-inverse'>
-                      <tr>
-                        <th colspan='2'><?php echo $r['name'] ?></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Business Address</td>
-                        <td><br/>
-                          <strong><?php echo $r['full_address'] ?></strong>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td align='justify'>Star Rating</td>
-                        <td><br/>
-                          <strong><?php echo $r['stars'] ?></strong>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-<?php
-            }
-          } else if (!empty($_GET['city'])) {
-            $cityName = $_GET['city'];
-            $query = array('city' => new MongoRegex("/$cityName/i"));
-            $result = $db->business->find($query)->sort(array('stars' => 1));
-
-            foreach ($result as $r) {
-?>
-              <div class="col-md-12" id="results">
-                <div class='panel panel-default'>
-                  <table class='table table-bordered'>
-                    <thead class='thead-inverse'>
-                      <tr>
-                        <th colspan='2'><?php echo $r['name'] ?></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>Business Address</td>
-                        <td><br/>
-                          <strong><?php echo $r['full_address'] ?></strong>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td align='justify'>Star Rating</td>
-                        <td><br/>
-                          <strong><?php echo $r['stars'] ?></strong>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-<?php
+              foreach ($result as $r) {
+                echo "<div class='panel panel-default'>";
+                echo "<table class='table table-bordered'>";
+                echo "<thead class='thead-inverse'>";
+                echo "<tr><th colspan='2'>".$r['name']."</th></tr></thead>";
+                echo "<tbody><tr><td>Business Address</td>";
+                echo "<td><br/><strong>".$r['full_address']."</strong></td></tr>";
+                echo "<tr><td align='justify'>Star Rating</td>";
+                echo "<td><br/><strong>".$r['stars']."</strong></td></tr>";
+                echo "<tr><td align='justify'>Link:</td>";
+                echo "<td><br/><strong><a href='https://twitter.com/search?q=".$keyWord."'>https://twitter.com/search?q=".$keyWord."</a></strong></td>";
+                echo "</tr></tbody></table></div>";
+              }
             }
           }
-?>
-      </div>
-      <div class="col-6-md" id="results">
-<?php
-        //put in the google maps code here
-?>
+        ?>
       </div>
     </div>
   </div>
