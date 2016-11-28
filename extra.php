d51a954c47b7bca72c9d31243a1942083e175788
Austin Underwood <sbaustin951@gmail.com>
2016-11-28 12:48:12 -0500

    reviews are showing now

--- extra.php
+++ extra.php
@@ -44,27 +44,8 @@
 					lng: '<?php echo $lng ?>',
 				});
 				<?php
-					$list= "";
 
-          $bid = $_GET['id'];
-					$query= array('business_id' => new MongoRegex("/$bid/"));
-					$result= $db->review->find($query);
-
-
-					foreach($result as $r){
-						// $user= "";
-						// $uQuery= array('user_id' => $r['user_id']);
-						// $userSet= $db->user->find($uQuery);
-						// foreach(userSet as $u){ $user= $u['name']; }
-
-						$list.= "<div class='panel panel-primary'>";
-						$list.= "<table class='table table-bordered'>";
-						$list.= "<thead class='thead-inverse'>";
-						$list.= "<tr><th class='well' colspan='2'>".$r['stars']."</th></tr></thead>";
-						$list.= "<tbody><tr><td>".$r['text']."</td></tr></table></div>";
-					}
-
-					echo "$('#reviews').html(\"".$list."\");\n";
+					//echo "$('#reviews').html(\"".$list."\");\n";
 				?>
 			});
 		</script>
@@ -86,7 +67,28 @@
 					<li><a data-toggle="tab" href="#streetview">Streetview</a></li>
 				</ul>
 				<div class="tab-content">
-					<div id="reviews" class="tab-pane fade in active" style="width: 600px; height: 550px"></div>
+					<div id="reviews" class="tab-pane fade in active" style="width: 600px; height: 550px">
+            <?php
+            $bid = $_GET['id'];
+  					$query= array('business_id' => new MongoRegex("/$bid/"));
+  					$result= $db->review->find($query);
+
+
+  					foreach($result as $r){
+  						// $user= "";
+  						// $uQuery= array('user_id' => $r['user_id']);
+  						// $userSet= $db->user->find($uQuery);
+  						// foreach(userSet as $u){ $user= $u['name']; }
+
+  						echo "<div class='panel panel-primary'>";
+  						echo "<table class='table table-bordered'>";
+  						echo "<thead class='thead-inverse'>";
+  						echo "<tr><th class='well' colspan='2'>".$r['stars']."</th></tr></thead>";
+              echo "<tr><th class='well' colspan='2'>".$r['date']."</th></tr></thead>";
+  						echo "<tbody><tr><td>".$r['text']."</td></tr></table></div>";
+  					}
+?>
+          </div>
 					<div id="streetview" class="tab-pane fade" style="width: 600px; height: 550px"></div>
 				</div>
 			</div>
